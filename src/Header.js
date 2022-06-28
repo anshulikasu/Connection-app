1a7d9fea0cac4be7971f1e4cb3c9c8b7fcca7862
Anshulika singh <95371754+anshulikasu@users.noreply.github.com>
2022-06-28 12:11:49 +0530

    Add files via upload

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className="header">
+    <div className="header-left">
+
+    </div>
+    <div className="center">
+    <i class="fi fi-rr-search"></i>
+    </div>
+    <div className="header-left">
+
+    </div>
+
+    </div>
+  )
+}
+
+export default Header
